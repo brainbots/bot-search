04ea662fdd6d0ca09582554796773bf58c6c7da5
Mohamed Seleem <seleem1337@users.noreply.github.com>
2017-07-05 19:49:56 +0200

    Follow new structure

--- chrome.py
+++ chrome.py
@@ -4,7 +4,7 @@ from functools import partial
 from PyQt5.QtCore import QProcess
 from pykeyboard import PyKeyboard
 
-from .abstract_bot import AbstractBot
+from bots.abstract_bot import AbstractBot
 from bots.action import Action
 from bots.utility import getWindow
 
